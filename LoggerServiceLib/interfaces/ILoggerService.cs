c74eff1cb82478fe16169404c63399c3ade732a9
Licheez <pierre@licheez.com>
2020-02-08 14:27:54 +0100

    MethodResult LoggerService

---
+++ LoggerServiceLib/interfaces/ILoggerService.cs
@@ -0,0 +1,41 @@
+﻿using pvWay.MethodResult.enums;
+using pvWay.MethodResult.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace pvWay.LoggerService.interfaces
+{
+    public interface ILoggerService : IDisposable
+    {
+        Guid? UserId { get; set; }
+        Guid? CompanyId { get; set; }
+        void Log(
+            string message = "passed",
+            SeverityEnum severity = SeverityEnum.Debug,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1);
+
+        void Log(
+            IEnumerable<string> messages,
+            SeverityEnum severity,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1);
+
+
+        void Log(
+            IMethodResult result,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1);
+
+        void Log(
+            Exception e,
+            SeverityEnum severity = SeverityEnum.Fatal,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1);
+    }
+}
