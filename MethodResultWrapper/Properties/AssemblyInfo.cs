31558bdf6e05321d0c31fe35bcd5db7382ea5b9d
Van Wallendael Pierre <pierre.vanwallendael@infrabel.be>
2020-02-26 11:11:12 +0100

    new empty project MethodResultWrapper (.net framework)

---
+++ MethodResultWrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MethodResultWrapper")]
+[assembly: AssemblyDescription("Provides a generic wrapper that returns whether or not a method succeeded of failed carrying the method result on success or a list of notifications/errors in case of failure")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("pvWay")]
+[assembly: AssemblyProduct("MethodResultWrapper")]
+[assembly: AssemblyCopyright("Copyright pvWay Ltd ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e1134c0e-b8ba-44e9-b4a1-9c73baeeb08a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
