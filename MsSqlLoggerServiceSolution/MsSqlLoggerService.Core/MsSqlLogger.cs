989e5016749e0fa1bebe43fbadc3554d0cdd6891
Licheez <pierre@licheez.com>
2020-05-27 17:04:49 +0200

    MsSqlLoggerService.Core

---
+++ MsSqlLoggerServiceSolution/MsSqlLoggerService.Core/MsSqlLogger.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using pvWay.MethodResultWrapper.Core;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace pvWay.MsSqlLoggerService.Core
+{
+    internal class ColumnInfo
+    {
+        public string ColumnName { get; }
+        public string Type { get; }
+        public int? Length { get; }
+        public bool IsNullable { get; }
+
+        public ColumnInfo(IDataRecord dr)
+        {
+            var iName = dr.GetOrdinal("column_name");
+            ColumnName = dr.GetString(iName);
+
+            var iDataType = dr.GetOrdinal("data_type");
+            Type = dr.GetString(iDataType);
+
+            var iLength = dr.GetOrdinal("character_maximum_length");
+
+            Length = dr.IsDBNull(iLength)
+                ? (int?)null : dr.GetInt32(iLength);
+
+            var iIsNullable = dr.GetOrdinal("is_nullable");
+            IsNullable = dr.GetString(iIsNullable) == "YES";
+        }
+    }
+
+    public class MsSqlLogger : ILoggerService
+    {
+        //private static volatile ILoggerService _instance;
+        //private static readonly object Locker = new object();
+
+        private readonly string _msSqlConnectionString;
+        private readonly string _tableSchema;
+        private readonly string _tableName;
+
+        private readonly SeverityEnum _logLevel;
+
+        private readonly string _userIdColumnName;
+        private int _userIdLength;
+
+        private readonly string _companyIdColumnName;
+        private int _companyIdLength;
+
+        private readonly string _topicColumnName;
+        private int _topicLength;
+
+        private readonly string _machineNameColumnName;
+        private int _machineNameLength;
+
+        private readonly string _severityCodeColumnName;
+
+        private readonly string _contextColumnName;
+        private int _contextLength;
+
+        private readonly string _messageColumnName;
+
+        private readonly string _createDateColumnName;
+
+        private string _userId;
+        private string _companyId;
+        private string _topic;
+
+        /// <summary>
+        /// Instantiates a new LoggerService.
+        /// This will check that the table exits and that all columns are compliant.
+        /// The constructor also store the max length of the columns so that the
+        /// info will be properly truncated when logging the data into the db.
+        /// The constructor throws a system exception when the table definition
+        /// does not comply to the requested pre-conditions.
+        /// </summary>
+        /// <param name="msSqlConnectionString"></param>
+        /// <param name="logLevel"></param>
+        /// <param name="tableSchema"></param>
+        /// <param name="tableName"></param>
+        /// <param name="userIdColumnName">name of the UserId column (should be varchar nullable)</param>
+        /// <param name="companyIdColumnName">name of the CompanyId column (should be varchar nullable)"</param>
+        /// <param name="machineNameColumnName">name of the MachineName column (should be varchar non nullable)</param>
+        /// <param name="severityCodeColumnName">name of the SeverityCode column (should be char non nullable)</param>
+        /// <param name="contextColumnName">name of the Context column (should be varchar non nullable)"</param>
+        /// <param name="topicColumnName">name of the Topic column (should be varchar nullable)</param>
+        /// <param name="messageColumnName">name of the Message column (should be varchar(MAX) non nullable)</param>
+        /// <param name="createDateColumnName">name of the CreateDateUtc column (should be datetime non nullable)</param>
+        public MsSqlLogger(
+            string msSqlConnectionString,
+            SeverityEnum logLevel,
+            string tableSchema,
+            string tableName,
+            string userIdColumnName,
+            string companyIdColumnName,
+            string machineNameColumnName,
+            string severityCodeColumnName,
+            string contextColumnName,
+            string topicColumnName,
+            string messageColumnName,
+            string createDateColumnName)
+        {
+            _msSqlConnectionString = msSqlConnectionString;
+            _logLevel = logLevel;
+            _tableSchema = tableSchema;
+            _tableName = tableName;
+            _userIdColumnName = userIdColumnName;
+            _companyIdColumnName = companyIdColumnName;
+            _machineNameColumnName = machineNameColumnName;
+            _severityCodeColumnName = severityCodeColumnName;
+            _contextColumnName = contextColumnName;
+            _topicColumnName = topicColumnName;
+            _messageColumnName = messageColumnName;
+            _createDateColumnName = createDateColumnName;
+            CheckTable();
+        }
+
+        private void CheckTable()
+        {
+            using (var cn = new SqlConnection(_msSqlConnectionString))
+            {
+                cn.Open();
+                var cmdText = "SELECT [column_name], " +
+                              "       [data_type], " +
+                              "       [is_nullable], " +
+                              "       [character_maximum_length] "
+                            + "FROM [information_schema].[columns] "
+                            + $"WHERE [table_schema] = '{_tableSchema}' " +
+                              $"AND   [table_name] = '{_tableName}'";
+
+                var cmd = cn.CreateCommand();
+                cmd.CommandText = cmdText;
+                cmd.CommandType = CommandType.Text;
+
+                var dic = new Dictionary<string, ColumnInfo>();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var ci = new ColumnInfo(reader);
+                    dic.Add(ci.ColumnName, ci);
+                }
+                reader.Close();
+                cn.Close();
+
+                var errors = new List<string>();
+                if (dic.Count == 0)
+                {
+                    errors.Add($"table {_tableName} not found");
+                }
+                else
+                {
+                    CheckColumn(errors, dic, _userIdColumnName, "varchar",
+                        true, out _userIdLength);
+                    CheckColumn(errors, dic, _companyIdColumnName, "varchar",
+                        true, out _companyIdLength);
+                    CheckColumn(errors, dic, _severityCodeColumnName, "char",
+                        false, out _);
+                    CheckColumn(errors, dic, _machineNameColumnName, "varchar",
+                        false, out _machineNameLength);
+                    CheckColumn(errors, dic, _topicColumnName, "varchar",
+                        true, out _topicLength);
+                    CheckColumn(errors, dic, _contextColumnName, "varchar",
+                        false, out _contextLength);
+                    CheckColumn(errors, dic, _messageColumnName, "nvarchar",
+                        false, out var len);
+                    if (len != -1)
+                    {
+                        errors.Add($"column {_messageColumnName} should be nvarchar(MAX)");
+                    }
+                    CheckColumn(errors, dic, _createDateColumnName, "datetime",
+                        false, out _);
+                }
+
+                if (errors.Any())
+                {
+                    var errorMessage = string.Empty;
+                    foreach (var error in errors)
+                    {
+                        if (!string.IsNullOrEmpty(errorMessage)) errorMessage += Environment.NewLine;
+                        errorMessage += error;
+                    }
+                    throw new Exception(errorMessage);
+                }
+            }
+        }
+
+        private static void CheckColumn(
+            ICollection<string> errors,
+            IDictionary<string, ColumnInfo> dic,
+            string columnName,
+            string expectedType,
+            bool isNullable,
+            out int length)
+        {
+            length = 0;
+            if (!dic.ContainsKey(columnName))
+            {
+                errors.Add($"{columnName} not found in log table");
+                return;
+            }
+            var info = dic[columnName];
+
+            var type = info.Type.ToLower();
+            expectedType = expectedType?.ToLower();
+            if (type != expectedType)
+            {
+                errors.Add($"{columnName} expected type is {expectedType} but actual type is {type}");
+                return;
+            }
+
+            length = info.Length ?? 0;
+
+            if (isNullable == info.IsNullable) return;
+
+            var neg = isNullable ? "" : "not ";
+            errors.Add($"{columnName} should {neg}be nullable");
+        }
+
+        public void SetUser(string userId, string companyId = null)
+        {
+            _userId = userId;
+            _companyId = companyId;
+        }
+
+        public void SetTopic(string topic)
+        {
+            _topic = topic;
+        }
+
+        public void Log(
+            string message,
+            SeverityEnum severity = SeverityEnum.Debug,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            Log(message, _topic, severity, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            IEnumerable<string> messages,
+            SeverityEnum severity,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            Log(messages, _topic, severity, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            IMethodResult res,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            Log(res, _topic, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            Exception e,
+            SeverityEnum severity = SeverityEnum.Fatal,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            Log(e, _topic, severity, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            string message,
+            string topic,
+            SeverityEnum severity = SeverityEnum.Debug,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            if (severity < _logLevel) return;
+            WriteLog(
+                severity,
+                topic,
+                $"{memberName} # {filePath} # {lineNumber}",
+                message);
+        }
+
+        public void Log(
+            IEnumerable<string> messages,
+            string topic,
+            SeverityEnum severity,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+
+            var errorMessage = string.Empty;
+            foreach (var message in messages)
+            {
+                if (!string.IsNullOrEmpty(errorMessage))
+                    errorMessage += Environment.NewLine;
+                errorMessage += message;
+            }
+
+            Log(errorMessage, topic, severity, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            IMethodResult res,
+            string topic,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            Log(res.ErrorMessage, topic, res.Severity, memberName, filePath, lineNumber);
+        }
+
+        public void Log(
+            Exception e,
+            string topic,
+            SeverityEnum severity = SeverityEnum.Fatal,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = -1)
+        {
+            var exception = GetDeepMessage(e);
+            var stackTrace = e.StackTrace;
+            var message = $"Exception: {exception}{Environment.NewLine}StackTrace: {stackTrace}";
+            Log(message, topic, severity, memberName, filePath, lineNumber);
+        }
+
+        private void WriteLog(
+            SeverityEnum severity, string topic, string context, string message)
+        {
+
+            using (var cn = new SqlConnection(_msSqlConnectionString))
+            {
+                cn.Open();
+
+                context = context?.Replace("'", "''") ?? "na";
+                if (context.Length > _contextLength)
+                    context = context.Substring(0, _contextLength);
+
+                // topic
+                string pTopic;
+                if (string.IsNullOrEmpty(topic))
+                {
+                    pTopic = "NULL";
+                }
+                else
+                {
+                    if (topic.Length > _topicLength)
+                        topic = topic.Substring(0, _topicLength);
+                    topic = topic.Replace("'", "''");
+                    pTopic = $"'{topic}'";
+                }
+
+                message = message?.Replace("'", "''") ?? "na";
+
+                // userId
+                string pUserId;
+                if (string.IsNullOrEmpty(_userId))
+                {
+                    pUserId = "NULL";
+                }
+                else
+                {
+                    if (_userId.Length > _userIdLength)
+                        _userId = _userId.Substring(0, _userIdLength);
+                    _userId = _userId.Replace("'", "''");
+                    pUserId = $"'{_userId}'";
+                }
+
+                // companyId
+                string pCompanyId;
+                if (string.IsNullOrEmpty(_companyId))
+                {
+                    pCompanyId = "NULL";
+                }
+                else
+                {
+                    if (_companyId.Length > _companyIdLength)
+                        _companyId = _companyId.Substring(0, _companyIdLength);
+                    _companyId = _companyId.Replace("'", "''");
+                    pCompanyId = $"'{_companyId}'";
+                }
+
+                var machineName = Environment.MachineName;
+                if (machineName.Length > _machineNameLength)
+                    machineName = machineName.Substring(0, _machineNameLength);
+                machineName = machineName.Replace("'", "''");
+
+                var cmdText = $"INSERT INTO [{_tableSchema}].[{_tableName}] "
+                              + "( "
+                              + $" [{_userIdColumnName}], "
+                              + $" [{_companyIdColumnName}], "
+                              + $" [{_severityCodeColumnName}], "
+                              + $" [{_machineNameColumnName}], "
+                              + $" [{_topicColumnName}], "
+                              + $" [{_contextColumnName}], "
+                              + $" [{_messageColumnName}], "
+                              + $" [{_createDateColumnName}] "
+                              + ")"
+                              + "VALUES "
+                              + "( "
+                              + $"{pUserId}, "
+                              + $"{pCompanyId}, "
+                              + $"'{EnumSeverity.GetCode(severity)}', "
+                              + $"'{machineName}', "
+                              + $"{pTopic}, "
+                              + $"'{context}', "
+                              + $"'{message}', "
+                              + $"'{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.sss}' "
+                              + ")";
+
+                var cmd = new SqlCommand(cmdText, cn)
+                {
+                    CommandType = CommandType.Text
+                };
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error : " + e.GetDeepMessage());
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+        }
+
+        private static string GetDeepMessage(Exception e)
+        {
+            var str = e.Message;
+            if (e.InnerException != null)
+                str = str + Environment.NewLine + GetDeepMessage(e.InnerException);
+            return str;
+        }
+
+        public void Dispose()
+        {
+            // nop
+        }
+    }
+}
---
+++ MsSqlLoggerServiceSolution/MsSqlLoggerService.Core/MsSqlLoggerServiceReadMe.md
@@ -0,0 +1,168 @@
+# MsSQLLoggerService for .Net Core
+## Version 1.0.0
+
+This service implements the ILoggerService from the **[MethodResultWrapper](https://www.nuget.org/packages/MethodResultWrapper/)** nuGet package using an Ms Sql database table as persistence layer
+
+
+### Pre-conditions for this service to work property
+
+The connection string provided should allow inserting rows
+into a table that should conform to the following DDL.
+
+### Log table definition example
+``` sql
+
+CREATE TABLE [dbo].[Log] (
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [varchar](36) NULL,
+    [CompanyId] [varchar](36) NULL, 
+	[SeverityCode] [char](1) NOT NULL, -- [D]Debug... [F] Fatal (see SeverityEnum)
+	[MachineName] [varchar](50) NOT NULL, -- Environment.MachineName
+	[Topic] varchar](50) NULL, -- enables to group log items for a given Topic
+	[Context] [varchar](256) NOT NULL, -- membername, filepath, line number...
+	[Message] [nvarchar](max) NOT NULL, -- the message
+	[CreateDateUtc] [datetime] NOT NULL, -- timestamp in universal central time
+
+	CONSTRAINT [PK_Log] 
+		PRIMARY KEY CLUSTERED ([Id] DESC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX 
+	ON TABLE [dbo].[Log] ([Topic] ASC)
+    WHERE [Topic] IS NOT NULL
+    
+```
+### Columns
+ 
+#### Id
+
+The Id column is not required and will not be populted by the service. However it might be convenient to have a primary column sorted DESC so that the last logs will be at the top of a select statement by default.
+
+If you define this column make sure the database will fill it accordingly by for example using the IDENTITY keyword
+
+#### UserId
+
+* You can provide your own column name for this column
+* The UserId column persists the identification of the connected user if any
+* This column should be nullable
+* This column should be of type varchar
+* The logger will truncate any info exceding the max column length
+
+#### CompanyId
+
+* You can provide your own column name for this column
+* The CompanyId column persists the identification of the company of the connected user if any
+* This column should be nullable
+* This column should be of type varchar
+* The logger will truncate any info exceding the max column length
+
+#### SeverityCode
+
+* You can provide your own column name for this column
+* The SeverityCode column persists the MethodResultWrapper.SeverityEnum code
+* This column should be non nullable
+* This column should be of type char (one char is enough)
+
+``` csharp
+   // Sevirity enum and corresponding codes
+   // -------------------------------------
+   public enum SeverityEnum
+    {
+        Ok, // "O"
+        Debug, // "D"
+        Info, // "I"
+        Warning, // "W"
+        Error, // "E"
+        Fatal, // "F"
+    }
+```
+
+#### MachineName
+
+*This column is certainly usefull in web farms*
+
+* You can provide your own column name for this column
+* The MachineName column persists Environment.MachineName
+* This column should be non nullable
+* This column should be of type varchar
+* The logger will truncate any info exceding the max column length
+
+#### Topic
+
+*This column lets you group logs for a given topic*
+
+* You can provide your own column name for this column
+* This column should be nullable
+* This column should be of type varchar
+* The logger will truncate any info exceding the max column length
+
+
+#### Context
+
+*Where does it happened*
+
+* You can provide your own column name for this column
+* The Context column persists method name, filepath and code line number.
+* This column should be non nullable.
+* This column should be of type varchar
+* The logger will truncate any info exceding the max column length
+ 
+#### Message
+
+*What happened*
+
+* You can provide your own column name for this column
+* The Message column persists the message info
+* This column should be non nullable.
+* This column should be of type nvarchar(MAX)
+
+#### CreateDateUtc
+
+*When does it happened*
+
+* You can provide your own column name for this column
+* The Message column persists the UTC date.
+* This column should be non nullable.
+* This column should be of type datetime
+
+### Usage
+
+```csharp
+using System;
+using pvWay.MethodResultWrapper;
+using pvWay.MsSqlLoggerService;
+
+namespace MsSqlLoggerServiceLab
+{
+    internal static class Program
+    {
+        private static void Main(/*string[] args*/)
+        {
+            const string cs = "data source=Localhost;initial catalog=iota_PRD_20200208;" +
+                              "integrated security=True;MultipleActiveResultSets=True;";
+
+            var ls = new Logger(
+                cs,
+                SeverityEnum.Debug,
+                "dbo",
+                "Log",
+                "UserId",
+                "CompanyId",
+                "MachineName",
+                "SeverityCode",
+                "Context",
+                "Topic",
+                "Message",
+                "CreateDateUtc");
+
+            ls.Log(new Exception());
+
+            Console.WriteLine("hit enter to quit");
+            Console.ReadLine();
+        }
+    }
+}
+
+```
