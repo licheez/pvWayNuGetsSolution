16ba4569fc4998a89e48ef87e9bde4116149aba8
Licheez <pierre@licheez.com>
2020-08-24 19:11:12 +0200

    add project MsSqlBackup.Core

---
+++ MsSqlBackupSolution/nc/MsSqlBackup.Core/IMsSqlBackupCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using pvWay.MethodResultWrapper.Core;
+
+namespace pvWay.MsSqlBackup.Core
+{
+    public interface IMsSqlBackupCreator
+    {
+        /// <summary>
+        /// Creates a full backup of the database
+        /// </summary>
+        /// <param name="bakFileName">Fully qualified backup file name where the running app has write access</param>
+        /// <returns>MethodResult see pvWay MethodResultWrapper nuGet package</returns>
+        Task<MethodResult> BackupDbAsync(string bakFileName);
+    }
+}
