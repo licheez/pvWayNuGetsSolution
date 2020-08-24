36f0d9404ba989f4a83fba01024bb241ee1070e5
Licheez <pierre@licheez.com>
2020-08-24 18:57:08 +0200

    MsSqlBackup dotNet framework project

---
+++ MsSqlBackupSolution/nf/MsSqlBackup/MsSqlBackupCreator.cs
@@ -0,0 +1,96 @@
+﻿using pvWay.MethodResultWrapper.Interfaces;
+using pvWay.MethodResultWrapper.Model;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace pvWay.MsSqlBackup
+{
+    public class MsSqlBackupCreator : IMsSqlBackupCreator
+    {
+        private readonly ILoggerService _ls;
+        private readonly string _localWorkFolder;
+        private readonly string _connectionString;
+        private readonly Action<string> _notify;
+        private readonly string _dataSource;
+        private readonly string _catalog;
+
+        private const string Notifier = "nuGet pvWay.MsSqlBackup";
+
+        /// <summary>
+        /// construct with (1) nuGet package pvWay MethodResultWrapper ILoggerService,
+        /// (2) a work folder where the Ms Sql Server is allowed to write,
+        /// (3) the connection string to the Db to backup and
+        /// (4) an optional callback that will get progress notifications
+        /// </summary>
+        /// <param name="ls">nuGet package pvWay MethodResultWrapper ILoggerService</param>
+        /// <param name="localWorkFolder">a place where the Ms Sql Server is allowed to write</param>
+        /// <param name="connectionString">the connection string to the Db to backup</param>
+        /// <param name="notify">optional callback that will get progress notifications</param>
+        public MsSqlBackupCreator(
+            ILoggerService ls,
+            string localWorkFolder,
+            string connectionString,
+            Action<string> notify = null)
+        {
+            _ls = ls;
+            _localWorkFolder = localWorkFolder.EndsWith("\\")
+                ? localWorkFolder : localWorkFolder + "\\";
+            _connectionString = connectionString;
+            _notify = notify;
+            var cs = new SqlConnectionStringBuilder(connectionString);
+            _dataSource = cs.DataSource;
+            _catalog = cs.InitialCatalog;
+        }
+
+        /// <summary>
+        /// Creates a full backup of the database
+        /// </summary>
+        /// <param name="bakFileName">Fully qualified backup file name where the running app has write access</param>
+        /// <returns>MethodResult see pvWay MethodResultWrapper nuGet package</returns>
+        public async Task<MethodResult> BackupDbAsync(string bakFileName)
+        {
+            var tmpFileName = $"{_localWorkFolder}tmp_{Guid.NewGuid()}.bak";
+            try
+            {
+
+                using (var cn = new SqlConnection(_connectionString))
+                {
+                    _notify?.Invoke($"{Notifier} connecting to {_dataSource}");
+                    await cn.OpenAsync();
+                    using (var cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandText = $"BACKUP DATABASE [{_catalog}] TO DISK='{tmpFileName}'";
+                        cmd.CommandType = CommandType.Text;
+
+                        _notify?.Invoke($"{Notifier} backing up database {_catalog} to local work folder {_localWorkFolder}");
+                        await cmd.ExecuteNonQueryAsync();
+
+                        _notify?.Invoke($"{Notifier} copying backup file to destination: {bakFileName}");
+                        File.Copy(tmpFileName, bakFileName);
+                    }
+
+                    cn.Close();
+
+                    return MethodResult.Ok;
+                }
+            }
+            catch (Exception e)
+            {
+                _ls.Log(e);
+                return new MethodResult(e);
+            }
+            finally
+            {
+                if (File.Exists(tmpFileName))
+                {
+                    _notify?.Invoke($"{Notifier} removing temp file from {_localWorkFolder}");
+
+                    File.Delete(tmpFileName);
+                }
+            }
+        }
+    }
+}
---
+++ MsSqlBackupSolution/nf/MsSqlBackup/readMe.md
@@ -0,0 +1,111 @@
+﻿# Ms Sql Backup
+
+Tiny DAO utility that backs up any Ms Sql Db (any edition) to a local or network drive.
+
+## Interface
+
+The nuGet has only one method
+
+```csharp
+using System.Threading.Tasks;
+using pvWay.MethodResultWrapper.Model;
+
+namespace pvWay.MsSqlBackup
+{
+    public interface IMsSqlBackupCreator
+    {
+        /// <summary>
+        /// Creates a full backup of the database
+        /// </summary>
+        /// <param name="bakFileName">Fully qualified backup file name where the running app has write access</param>
+        /// <returns>MethodResult see pvWay MethodResultWrapper nuGet package</returns>
+        Task<MethodResult> BackupDbAsync(string bakFileName);
+    }
+}
+```
+
+## Usage
+
+See here after a short Console that use the service
+
+### Principe
+
+* need to pass a work folder where the Ms Sql Server has write access
+* need to pass the connection string
+* need to pass a destination file where the app has write access
+* you can follow up on progress by passing a notifier callback
+
+remark: you'll need to get the [MethodResultWrapper](https://github.com/licheez/pvWayNuGetsSolution/tree/master/MethodResultWrapperSolution/MethodResultWrapper) to use this service
+
+### The code
+
+```csharp
+
+using System;
+using pvWay.MethodResultWrapper.Model;
+using pvWay.MsSqlBackup;
+
+namespace MsSqlBackupLab
+{
+    internal static class Program
+    {
+        private static void Main(/*string[] args*/)
+        {
+            // need to pass the ILoggerService from pvWay MethodResultWrapper nuGet package
+            // her I use the ConsoleLogger implementation
+            // see https://github.com/licheez/pvWayNuGetsSolution/tree/master/MethodResultWrapperSolution/MethodResultWrapper
+            var ls = new ConsoleLogger();
+
+            // make sure Ms Sql Server has write access to this work folder
+            const string localWorkFolder = "d:\\temp";
+
+            // the connection string to the db you want to back up
+            const string connectionString = "data source = localhost; " +
+                                            "initial catalog = TCM004_DEV; " +
+                                            "integrated security = True; " +
+                                            "MultipleActiveResultSets = True; ";
+
+            var backupCreator = new MsSqlBackupCreator(
+                ls,
+                localWorkFolder,
+                connectionString,
+                // let's redirect progress notifications to the console
+                Console.WriteLine);
+
+            // let's generate a unique bak file name using a GUID
+            var id = Guid.NewGuid().ToString();
+            var bakFileNameNwDrive = $"y:\\MsSqlBak\\Tcm004_{id}.bak";
+
+            // ok now launch the backup
+            var res = backupCreator
+                .BackupDbAsync(bakFileNameNwDrive).Result;
+
+            /*
+            
+            CONSOLE Should display this
+            ---------------------------
+            nuGet pvWay.MsSqlBackup connecting to localhost
+            nuGet pvWay.MsSqlBackup backing up database TCM004_DEV to local work folder d:\temp\
+            nuGet pvWay.MsSqlBackup copying backup file to destination: y:\MsSqlBak\Tcm004_85daa4de-d2b3-42e8-99bb-aad2252f1b41.bak
+            nuGet pvWay.MsSqlBackup removing temp file from d:\temp
+
+            */
+
+            if (res.Failure)
+            {
+                ls.Log(res);
+            }
+            else
+            {
+                Console.WriteLine("Backup completed");
+            }
+
+            Console.WriteLine("hit a key to quit");
+            Console.ReadKey();
+        }
+    }
+}
+
+```
+
+Happy coding
