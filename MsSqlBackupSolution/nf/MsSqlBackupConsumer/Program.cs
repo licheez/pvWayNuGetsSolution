fd5c0c2155a608279733a73603f941f66ae1b926
Licheez <pierre@licheez.com>
2020-08-26 13:05:56 +0200

    fix wrong package submitted

---
+++ MsSqlBackupSolution/nf/MsSqlBackupConsumer/Program.cs
@@ -0,0 +1,63 @@
+﻿using pvWay.MethodResultWrapper.Model;
+using pvWay.MsSqlBackup;
+using System;
+
+namespace MsSqlBackupConsumer
+{
+    internal static class Program
+    {
+        private static void Main(/*string[] args*/)
+        {
+            // need to pass the ILoggerService from pvWay MethodResultWrapper nuGet package
+            // her I use the ConsoleLogger implementation
+            // see https://github.com/licheez/pvWayNuGetsSolution/tree/master/MethodResultWrapperSolution/MethodResultWrapper
+            var ls = new ConsoleLogger();
+
+            // make sure Ms Sql Server has write access to this work folder
+            const string localWorkFolder = "d:\\temp";
+
+            // the connection string to the db you want to back up
+            const string connectionString = "data source = localhost; " +
+                                            "initial catalog = TCM004_DEV; " +
+                                            "integrated security = True; " +
+                                            "MultipleActiveResultSets = True; ";
+
+            var backupCreator = new MsSqlBackupCreator(
+                ls,
+                localWorkFolder,
+                connectionString,
+                // let's redirect progress notifications to the console
+                Console.WriteLine);
+
+            // let's generate a unique bak file name using a GUID
+            var id = Guid.NewGuid().ToString();
+            var bakFileNameNwDrive = $"d:\\msSqlBak\\Tcm004_{id}.bak";
+
+            // ok now launch the backup
+            var res = backupCreator.BackupDb(bakFileNameNwDrive);
+
+            /*
+            
+            CONSOLE Should display this
+            ---------------------------
+            nuGet pvWay.MsSqlBackup connecting to localhost
+            nuGet pvWay.MsSqlBackup backing up database TCM004_DEV to local work folder d:\temp\
+            nuGet pvWay.MsSqlBackup copying backup file to destination: y:\MsSqlBak\Tcm004_85daa4de-d2b3-42e8-99bb-aad2252f1b41.bak
+            nuGet pvWay.MsSqlBackup removing temp file from d:\temp
+
+            */
+
+            if (res.Failure)
+            {
+                ls.Log(res);
+            }
+            else
+            {
+                Console.WriteLine("Backup completed");
+            }
+
+            Console.WriteLine("hit a key to quit");
+            Console.ReadKey();
+        }
+    }
+}
--- MsSqlBackupSolution/nf/MsSqlBackupConsumer/Properties/AssemblyInfo.cs
+++ MsSqlBackupSolution/nf/MsSqlBackupConsumer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MsSqlBackup")]
-[assembly: AssemblyDescription("Backup Ms SQL database (any edition)")]
+[assembly: AssemblyTitle("MsSqlBackupConsumer")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("pvWay")]
-[assembly: AssemblyProduct("MsSqlBackup")]
-[assembly: AssemblyCopyright("Copyright pvWay Ltd ©  2020")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MsSqlBackupConsumer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2949f6a9-9215-46a9-8e03-ec14b016d728")]
+[assembly: Guid("b33ea158-5425-4397-89cd-17b3ea7bd460")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
