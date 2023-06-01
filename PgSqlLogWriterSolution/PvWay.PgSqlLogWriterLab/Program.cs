d18450343ff3620b6d63895cf946f3de4ced23a2
pvWay <pierre@pvway.com>
2023-06-01 18:59:57 +0200

    pgSqlLogWriter (initial)

---
+++ PgSqlLogWriterSolution/PvWay.PgSqlLogWriterLab/Program.cs
@@ -0,0 +1,17 @@
+﻿using PvWay.PgSqlLogWriter.nc6;
+
+
+Console.WriteLine("Hello, PgSqlLogWriter");
+
+const string cs = "Server=localhost;" +
+                  "Database=postgres;" +
+                  "User Id=sa;" +
+                  "Password=S0mePwd_;";
+
+var lw = new PgSqlLogWriter(
+    async () => await Task.FromResult(cs));
+
+lw.WriteLog("userId", "companyId", "topic", "D",
+    Environment.MachineName, "member", "file", 1,
+    "message", DateTime.UtcNow);
+
