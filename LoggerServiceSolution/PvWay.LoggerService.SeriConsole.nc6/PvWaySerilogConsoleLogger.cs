6d51b39b672b9676fe256fb8593c755d660add86
pierre <pierre@pvway.com>
2024-08-03 07:25:11 +0200

    Add SeriConsole project to solution
    Introduced a new project "PvWay.LoggerService.SeriConsole.nc6" to the solution. This project implements ILoggerService using Serilog for console logging. Configuration and usage details are included in the new project files.

---
+++ LoggerServiceSolution/PvWay.LoggerService.SeriConsole.nc6/PvWaySerilogConsoleLogger.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using PvWay.LoggerService.Abstractions.nc6;
+using PvWay.LoggerService.nc6;
+
+namespace PvWay.LoggerService.SeriConsole.nc6;
+
+public static class PvWaySerilogConsoleLogger
+{
+    public static ISeriConsoleLoggerService Create(
+        SeverityEnu minLogLevel = SeverityEnu.Trace)
+    {
+        return new SerilogConsoleService(
+            new LoggerServiceConfig(minLogLevel));
+    }
+
+    public static ISeriConsoleLoggerService<T> Create<T>(
+        SeverityEnu minLogLevel = SeverityEnu.Trace)
+    {
+        return new SerilogConsoleService<T>(
+            new LoggerServiceConfig(minLogLevel));
+    }
+    
+    public static void AddPvWaySeriConsoleLoggerService(
+        this IServiceCollection services,
+        SeverityEnu minLogLevel = SeverityEnu.Trace,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    {
+        services.TryAddSingleton<ILoggerServiceConfig>(_ =>
+            new LoggerServiceConfig(minLogLevel));
+        
+        var sd = new ServiceDescriptor(
+            typeof(ILoggerService),
+            typeof(SerilogConsoleService),
+            lifetime);
+        services.Add(sd);
+        
+        var sd2 = new ServiceDescriptor(
+            typeof(ISeriConsoleLoggerService),
+            typeof(SerilogConsoleService),
+            lifetime);
+        services.Add(sd2);
+    }
+    
+}
---
+++ LoggerServiceSolution/PvWay.LoggerService.SeriConsole.nc6/SerilogConsoleService.cs
@@ -0,0 +1,22 @@
+using PvWay.LoggerService.Abstractions.nc6;
+using PvWay.LoggerService.nc6;
+
+namespace PvWay.LoggerService.SeriConsole.nc6;
+
+internal sealed class SerilogConsoleService : 
+    LoggerService.nc6.LoggerService,
+    ISeriConsoleLoggerService
+{
+    public SerilogConsoleService(ILoggerServiceConfig config) : base(new SerilogConsoleWriter(), config)
+    {
+    }
+}
+
+internal sealed class SerilogConsoleService<T> : 
+    LoggerService<T>,
+    ISeriConsoleLoggerService<T>
+{
+    public SerilogConsoleService(ILoggerServiceConfig config) : base(new SerilogConsoleWriter(), config)
+    {
+    }
+}
