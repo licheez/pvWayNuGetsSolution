186ca52559c32977dc4b23f86ce2e8e379cfa86d
pierre <pierre@pvway.com>
2024-08-04 10:48:09 +0200

    Add PgSqlLoggerServiceFactory and refactor logger setup
    Introduced PgSqlLoggerServiceFactory for better logger service creation. Refactored PvWayPgSqlLogger to split creation methods and enhanced dependency injection setup. Updated project version and adjusted packaging paths.

--- LoggerServiceSolution/PvWay.LoggerService.PgSql.nc6/PvWayPgSqlLogger.cs
+++ LoggerServiceSolution/PvWay.LoggerService.PgSql.nc6/PvWayPgSqlLogger.cs
@@ -8,7 +8,8 @@ namespace PvWay.LoggerService.PgSql.nc6;
 
 public static class PvWayPgSqlLogger
 {
-    public static IPgSqlLoggerService Create(
+    // CREATORS
+    public static IPgSqlLogWriter CreateWriter(
         Func<SqlRoleEnu, Task<string>> getCsAsync,
         SeverityEnu minLogLevel = SeverityEnu.Trace,
         string? schemaName = "public",
@@ -22,45 +23,78 @@ public static class PvWayPgSqlLogger
         string? messageColumnName = "Message",
         string? createDateUtcColumnName = "CreateDateUtc")
     {
-        return new PgSqlLoggerService(
-            new LoggerServiceConfig(minLogLevel), 
-            new PgSqlLogWriter(
-            new PgSqlConnectionStringProvider(getCsAsync), 
+        return new PgSqlLogWriter(
+            new PgSqlConnectionStringProvider(getCsAsync),
             new PgSqlLogWriterConfig(
-                schemaName, tableName, 
-                userIdColumnName, companyIdColumnName, 
-                machineNameColumnName, severityCodeColumnName, 
-                contextColumnName, topicColumnName, 
-                messageColumnName, createDateUtcColumnName)));
+                schemaName, tableName,
+                userIdColumnName, companyIdColumnName,
+                machineNameColumnName, severityCodeColumnName,
+                contextColumnName, topicColumnName,
+                messageColumnName, createDateUtcColumnName));
+    }
+    
+    public static IPgSqlLoggerService CreateService(
+        IPgSqlLogWriter logWriter,
+        SeverityEnu minLogLevel = SeverityEnu.Trace)
+    {
+        var config = new LoggerServiceConfig(minLogLevel);
+        return new PgSqlLoggerService(config, logWriter);
     }
 
-    public static IPgSqlLoggerService<T> Create<T>(
-        Func<SqlRoleEnu, Task<string>> getCsAsync,
-        SeverityEnu minLogLevel = SeverityEnu.Trace,
-        string? schemaName = "public",
-        string? tableName = "Log",
-        string? userIdColumnName = "UserId",
-        string? companyIdColumnName = "CompanyId",
-        string? machineNameColumnName = "MachineName",
-        string? severityCodeColumnName = "SeverityCode",
-        string? contextColumnName = "Context",
-        string? topicColumnName = "Topic",
-        string? messageColumnName = "Message",
-        string? createDateUtcColumnName = "CreateDateUtc")
+    public static IPgSqlLoggerService<T> CreateService<T>(
+        IPgSqlLogWriter logWriter,
+        SeverityEnu minLogLevel = SeverityEnu.Trace)
     {
-        return new PgSqlLoggerService<T>(
-            new LoggerServiceConfig(minLogLevel), 
-            new PgSqlLogWriter(
-            new PgSqlConnectionStringProvider(getCsAsync), 
-            new PgSqlLogWriterConfig(
-                schemaName, tableName, 
-                userIdColumnName, companyIdColumnName, 
-                machineNameColumnName, severityCodeColumnName, 
-                contextColumnName, topicColumnName, 
-                messageColumnName, createDateUtcColumnName)));
+        var config = new LoggerServiceConfig(minLogLevel);
+        return new PgSqlLoggerService<T>(config, logWriter);
     }
     
+    // LOG WRITER
+    public static void AddPvWayPgSqlLogWriter(
+        this IServiceCollection services,
+        Func<SqlRoleEnu, Task<string>> getCsAsync,
+        IConfiguration? lwConfig = null)
+    {
+        var csp = new PgSqlConnectionStringProvider(getCsAsync);
+        var config = new PgSqlLogWriterConfig(lwConfig);
+        services.TryAddSingleton<IPgSqlLogWriter>(_ =>
+            new PgSqlLogWriter(csp, config));
+    }
     
+    // FACTORY
+    public static void AddPvWayPgSqlLoggerServiceFactory(
+        this IServiceCollection services,
+        IConfiguration config,
+        Func<SqlRoleEnu, Task<string>> getCsAsync,
+        SeverityEnu minLogLevel = SeverityEnu.Trace)
+    {
+        services.TryAddSingleton<ILoggerServiceFactory<IPgSqlLoggerService>>(
+            _ =>
+                new PgSqlLoggerServiceFactory(getCsAsync, config, minLogLevel));
+    }
+    
+   // SERVICES
+   /// <summary>
+   /// Use this injector if you already injected the IPgSqlLogWriter
+   /// </summary>
+   /// <param name="services"></param>
+   /// <param name="minLogLevel"></param>
+   /// <param name="lifetime"></param>
+   public static void AddPvWayPgSqlLoggerService(
+       this IServiceCollection services,
+       SeverityEnu minLogLevel = SeverityEnu.Trace,
+       ServiceLifetime lifetime = ServiceLifetime.Scoped)
+   {
+       services.TryAddSingleton<ILoggerServiceConfig>(_ =>
+           new LoggerServiceConfig(minLogLevel));
+        
+       var sd = new ServiceDescriptor(
+           typeof(IPgSqlLoggerService<>),
+           typeof(PgSqlLoggerService<>),
+           lifetime);
+       services.Add(sd);
+   }
+   
     public static void AddPvWayPgSqlLoggerService(
         this IServiceCollection services,
         Func<SqlRoleEnu, Task<string>> getCsAsync,
@@ -77,22 +111,9 @@ public static class PvWayPgSqlLogger
                 new PgSqlLogWriterConfig(lwConfig)));
 
         var sd = new ServiceDescriptor(
-            typeof(ILoggerService),
+            typeof(IPgSqlLogWriter),
             typeof(PgSqlLoggerService),
             lifetime);
         services.Add(sd);
-        
-        var sd2 = new ServiceDescriptor(
-            typeof(IPgSqlLoggerService),
-            typeof(PgSqlLoggerService),
-            lifetime);
-        services.Add(sd2);
-        
-        var sd3 = new ServiceDescriptor(
-            typeof(ISqlLoggerService),
-            typeof(PgSqlLoggerService),
-            lifetime);
-        services.Add(sd3);
-        
     }
 }
